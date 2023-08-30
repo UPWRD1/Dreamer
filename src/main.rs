96866746f984365b1561697c274766f1b3dc7b46
Luke Davis <luke.n.davis.7@gmail.com>
2023-08-30 13:08:40 -0500

    Finished file creation, handled edge cases.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
-use std::env;
+use std::env::{self};
+use std::iter::*;
 use std::fs::File;
 use std::io::*;
 //use std::io::prelude::*;
 
-pub fn run<String: std::fmt::Debug>(argslist: Vec<String>) {
-    
+//extern crate yaml_rust;
+
+pub fn run<String: std::fmt::Debug>(argsv: Vec<String>) {
+    println!("{:?}", argsv);
 }
 
-pub fn new<String: std::fmt::Debug>(argslist: Vec<String>) {
-    println!("New pipe: {:?}", argslist[2]);
-    let _pipe_name = &argslist[2];
-    let mut file = File::create("myPipe.plu.yaml").expect("Error encountered while creating file!");
-    file.write_all(b"do: { \n \t echo hello world!\n }").expect("Error while writing to file");
+pub fn new(argsv: Vec<String>) {
+    if argsv.len() < 3 {
+        panic!("\n Not enough arguments! Usage: \n \t plumber new <pipename>")
+    }
+    let plufile_name: String = format!("{}.plu.yaml", &argsv[2]);
+    println!("    ~> New pipe: {}", plufile_name);
+    let mut plufile = File::create(plufile_name).expect("Error encountered while creating file!");
+    plufile.write_all(b"do: { \n \t echo hello world!\n }").expect("Error while writing to file");
 }
 
 pub fn argparse(argsv: Vec<String>, pos: usize, item: String) -> bool {
+    // Parse arguments
     if argsv.len() > 1 && argsv[pos] == item {
         return true;
     } else {
@@ -23,21 +30,18 @@ pub fn argparse(argsv: Vec<String>, pos: usize, item: String) -> bool {
 }
 
 pub fn cli() {
-    let args: Vec<String> = env::args().collect();
+    // Main cli function
+    let args: Vec<String> = env::args().collect(); // Argument collection
+    // Parsing
     if argparse(args.clone(), 1, "new".to_string()) {
-        new(args);
+        new(args); // Create new plufile
     } else if argparse(args.clone(), 1, "run".to_string()) {
-        run(args);
-    }
-/*
-    if args.len() > 1 && args[1] == "new" {
-        new(args)
+        run(args); // Run plufile
     } else {
-        println!("Invalid");
+        println!("Invalid Argument");
     }
-    */
 }
 
 fn main() {
-    cli()
+    cli();
 }
