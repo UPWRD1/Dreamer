a01a976ed75af346f213caec93123e1418deb718
Luke Davis <luke.n.davis.7@gmail.com>
2023-10-19 09:33:02 -0500

    Fixed Clippy warnings

--- src/helper/wizards/mod.rs
+++ src/helper/wizards/mod.rs
@@ -4,12 +4,12 @@ use super::{colored::Colorize, input_fmt, resource::print_file_list, continue_pr
 
 use std::error::Error;
 
-pub fn init_cmd_wizard(global_opts: &[bool]) -> Result<String, ()> {
+pub fn init_cmd_wizard(global_opts: &[bool]) -> String {
     let filename = questionprint!("Enter a name for your project:");
     infoprint!("Your file will be created as {}.uni.yaml.", filename);
     continue_prompt(global_opts);
     let filename_f = format!("{}.uni.yaml", filename);
-    Ok(filename_f)
+    filename_f
 }
 
 pub fn add_cmd_wizard() -> Result<(String, String), Box<dyn Error>> {
